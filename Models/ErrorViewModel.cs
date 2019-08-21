f11990ead2a5c55857d2111d8636cf8d4c56f9ab
Sakul <au.perspectives@gmail.com>
2019-08-21 13:55:01 +0700

    Create new we project with MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demo_AzureDay.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
